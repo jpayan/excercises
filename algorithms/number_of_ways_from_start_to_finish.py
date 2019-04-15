da9f6765d23e0a08c52b9359e7099c26d2080f87
Payan, Jorge <jorge.payan@thermofisher.com>
2019-04-15 14:24:07 -0700

    delete math methods

--- algorithms/number_of_ways_from_start_to_finish.py
+++ algorithms/number_of_ways_from_start_to_finish.py
@@ -24,21 +24,16 @@
 # Moving Right and reached [*][m-1]
 # Moving Down and reached [n-1][*]
 
-w = 0
-z = 0
-
-
-def number_of_unique_paths_logic(height: int, width: int) -> int:
-    return _number_of_unique_paths(height, width)
-
 
 # O(((m - 1) + (n - 1) C (m - 1)) * 2 - 1)
-def _number_of_unique_paths(height: int, width: int, x: int=0, y: int=0) -> int:
-    global w
-    w += 1
+def number_of_unique_paths_recursive(height: int, width: int) -> int:
+    return _number_of_unique_paths_recursive(height, width, 0, 0)
+
+
+def _number_of_unique_paths_recursive(height: int, width: int, x: int, y: int) -> int:
     if x == height - 1 or y == width - 1:
         return 1
-    return _number_of_unique_paths(height, width, x + 1, y) + _number_of_unique_paths(height, width, x, y + 1)
+    return _number_of_unique_paths_recursive(height, width, x + 1, y) + _number_of_unique_paths_recursive(height, width, x, y + 1)
 
 
 # O((2(m - 1) + 2(n - 1)) * O() of multiplication))
@@ -48,35 +43,14 @@ def number_of_unique_paths_math(height: int, width: int) -> int:
 
 def factorial(n: int) -> int:
     res = 1
-    for i in range(1, n):
-        res += product(res, i)
+    for i in range(1, n + 1):
+        res *= i
     return res
-
-
-def product(a, b):
-    global z
-    res = 0
-    for _ in range(0, b):
-        z += 1
-        res = addition(res, a)
-    return res
-
-
-def addition(a, b):
-    global z
-    while b:
-        z += 1
-        carry = a & b
-        a = a ^ b
-        b = carry << 1
-    return a
 
 
 if __name__ == '__main__':
     height = 5
     width = 5
 
-    print(number_of_unique_paths_logic(height, width))
-    print(w)
-    # print(number_of_unique_paths_math(height, width))
-    # print(z)
+    print(number_of_unique_paths_recursive(height, width))
+    print(number_of_unique_paths_math(height, width))
