d57404cff976e8deb5a3045f8b504029c4b59485
Payan, Jorge <jorge.payan@thermofisher.com>
2019-04-15 12:39:21 -0700

    change history to distances

--- algorithms/shortest_path_in_steps_array.py
+++ algorithms/shortest_path_in_steps_array.py
@@ -15,36 +15,23 @@ def breadth_first_search(graph: List[int], source_vertex: int, target_vertex: in
     vertices_to_visit = Queue()
     vertices_to_visit.enqueue(source_vertex)
 
-    # distances = {source_vertex: 0}  # Use this to get number of steps
-    history = {source_vertex: None}  # Use this to get path
+    distances = {source_vertex: 0}  # Use this to get number of steps
 
     while not vertices_to_visit.is_empty():
         current_vertex = vertices_to_visit.dequeue()
         for neighbor in range(1, graph[current_vertex] + 1):
             neighbor_index = current_vertex + neighbor
             if neighbor_index < len(graph):
-                # if neighbor_index not in distances:
-                if neighbor_index not in history:
-                    # distances[neighbor_index] = distances[current_vertex] + 1
-                    history[neighbor_index] = current_vertex
+                if neighbor_index not in distances:
+                    distances[neighbor_index] = distances[current_vertex] + 1
                     if neighbor_index == target_vertex:
-                        # return distances[target_vertex]
-                        return get_path(history, target_vertex)
+                        return distances[target_vertex]
                     vertices_to_visit.enqueue(neighbor_index)
 
 
-def get_path(history: Dict[int, int], target_vertex: int):
-    path = []
-    if target_vertex in history:
-        while target_vertex is not None:
-            path.insert(0, target_vertex)
-            target_vertex = history[target_vertex]
-
-    return path
-
-
 if __name__ == '__main__':
-    max_steps = 100
-    max_nodes = 1000
+    max_steps = 5
+    max_nodes = 10
     steps = [randint(1, max_steps) for i in range(max_nodes)]
+    print(steps)
     print(get_least_amount_of_steps(steps))
